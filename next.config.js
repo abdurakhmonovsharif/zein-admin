119fb2e33715a7650f333b1b74f4a778345897c2
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2025-05-24 23:10:11 +0500

    updated next and ts config files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

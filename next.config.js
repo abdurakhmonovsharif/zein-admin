1c9ea038d0d820a6d0445de1010b99922e19174e
Abdurakhmonov Sharif <sharifabduraxmonov@mail.ru>
2025-05-24 23:50:37 +0500

    update next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,26 @@
 const path = require("path");
+const { loadEnvConfig } = require('@next/env');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  },
-  devServer: {
-    port: 4000
-  }
+const nextConfig = () => {
+  // Load environment variables based on NODE_ENV
+  loadEnvConfig(__dirname);
+
+  return {
+    output: 'standalone',
+    reactStrictMode: true,
+    webpack: (config) => {
+      config.resolve.alias['@'] = path.resolve(__dirname);
+      return config;
+    },
+    devServer: {
+      port: 4000
+    },
+    env: {
+      // Expose API URL to client side
+      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+    }
+  };
 };
 
 module.exports = nextConfig;
